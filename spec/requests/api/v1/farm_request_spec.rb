22ff93ec5fafc68ee5d990f45e327b476247e7b4
Philip Medlin <phmedlin@gmail.com>
2022-01-06 20:46:06 -0700

    SimpleCov

--- spec/requests/api/v1/farm_request_spec.rb
+++ spec/requests/api/v1/farm_request_spec.rb
@@ -14,4 +14,14 @@ describe 'Farm API' do
 
     expect(farm[:attributes][:name]).to be_a(String)
   end
+
+  it 'can create a new farm' do
+    user_id = create(:user).id
+
+    post "/api/v1/farms?user_id=#{user_id}"
+
+    farm = (JSON.parse(response.body, symbolize_names: true))[:data]
+
+    expect(farm[:attributes][:user_id]).to eq(user_id)
+  end
 end
