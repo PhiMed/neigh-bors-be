92fe918d0e77981af823b9b57285ae8d5a8e81c5
Lesley Sanders <lesleyasanders@gmail.com>
2022-01-07 10:12:25 -0500

    Update farm request spec

--- spec/requests/api/v1/farm_request_spec.rb
+++ spec/requests/api/v1/farm_request_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
 describe 'Farm API' do
+  it 'can find all farms' do
+    farm_1 = create(:farm)
+    farm_2 = create(:farm)
+    farm_3 = create(:farm)
+
+    get "/api/v1/farms"
+
+    expect(response).to be_successful
+  end
 
   it "can get one farm by its id" do
-
     id = create(:farm).id
 
     get "/api/v1/farms/#{id}"
@@ -15,6 +23,14 @@ describe 'Farm API' do
     expect(farm[:attributes][:name]).to be_a(String)
   end
 
+  it 'sends an error code if farm does not exist' do
+    farm_1 = create(:farm)
+
+    get "/api/v1/farms/100"
+
+    expect(response.status).to eq(404)
+  end
+
   it 'can create a new farm' do
     user_id = create(:user).id
 
@@ -23,5 +39,21 @@ describe 'Farm API' do
     farm = (JSON.parse(response.body, symbolize_names: true))[:data]
 
     expect(farm[:attributes][:user_id]).to eq(user_id)
+  end
+
+  it 'sends an error code if farm is not created' do
+    user_1 = create(:user)
+
+    post "/api/v1/farms?user_id=100"
+
+    expect(response.status).to eq(400)
+  end
+
+  it 'can update a farm' do
+    user_id = create(:user).id
+
+    patch "/api/v1/farms?user_id=#{user_id}"
+
+    expect(farm[:attributes][:user_id]).to eq(user_1.id)
   end
 end
