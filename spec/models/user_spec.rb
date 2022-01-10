7be702e722f5883e80619730e41a0cc046c408c7
Didi Dodson <mdodson55@gmail.com>
2022-01-10 15:18:23 -0700

    Add user validations and testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe 'relationships' do
+    it { should have_one(:farm) }
+  end
 
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:phone) }
+    it { should validate_presence_of(:rescuer_trailer_capacity) }
+    it { should validate_presence_of(:address) }
+  end
 end
